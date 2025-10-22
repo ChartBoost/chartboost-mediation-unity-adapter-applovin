231b8cc30f7253902a53b7af9659f04126cc58c3
helium-service <helium-service@chartboost.com>
2025-10-22 15:54:03 -0700

    [AUTO-GENERATED] Synchronizing changes from private repository

--- Runtime/AppLovin/AppLovinAdapter.cs
+++ Runtime/AppLovin/AppLovinAdapter.cs
@@ -13,7 +13,7 @@ namespace Chartboost.Mediation.AppLovin
         /// <summary>
         /// The partner adapter Unity version.
         /// </summary>
-        public const string AdapterUnityVersion = "5.3.3";
+        public const string AdapterUnityVersion = "5.3.4";
         
         /// <inheritdoc cref="IPartnerAdapterConfiguration.AdapterNativeVersion"/>
         public static string AdapterNativeVersion => Instance.AdapterNativeVersion;
